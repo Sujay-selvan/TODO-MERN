3f3f27474e55964d36edd2589da195cc567e3e16
sujay-selvan <sujayu192015@gmail.com>
2025-03-17 13:31:54 +0530

    8th

--- front-end/crud/vite.config.js
+++ front-end/crud/vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
+import envCompatible from 'vite-plugin-env-compatible'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    envCompatible()
+  ],
+  envPrefix:"REACT_APP_",
   build: {
     outDir: 'dist', 
   }
